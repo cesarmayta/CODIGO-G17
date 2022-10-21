77d3b6018278bb19470e4d2ced5659666191223b
César Mayta <cesarmayta@gmail.com>
2022-10-20 22:44:45 -0500

    videojuego paleta

--- semana01/dia4/videojuego/main.py
+++ semana01/dia4/videojuego/main.py
@@ -20,10 +20,33 @@ class Bolita(pygame.sprite.Sprite):
         self.speed = [3,3]
         
     def update(self):
+        #verificar si la bolita se sale de la pantalla
+        if self.rect.bottom >= ALTO or self.rect.top <= 0:
+            self.speed[1] = -self.speed[1]
+        elif self.rect.right >= ANCHO or self.rect.left <= 0:
+            self.speed[0] = -self.speed[0]
+            
         #movemos la bolita
         self.rect.move_ip(self.speed)
 
-
+class Paleta(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('imagenes/paleta.png')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (ANCHO / 2,ALTO -20)
+        self.speed = [0,0]
+        
+    def update(self,evento):
+        if evento.key == pygame.K_LEFT and self.rect.left > 0:
+            self.speed = [-5,0]
+        elif evento.key == pygame.K_RIGHT and self.rect.right < ANCHO:
+            self.speed = [5,0]
+        else:
+            self.speed = [0,0]
+            
+        self.rect.move_ip(self.speed)
 ######################################
 
 #creamos la pantalla del videojuego
@@ -33,8 +56,12 @@ pygame.display.set_caption('Mi primer videjuego con Python')
 #crear un reloj para hacer que bolita se mueva mas lento
 reloj = pygame.time.Clock()
 
+#ajustar repetición del evento de tecla presionada
+pygame.key.set_repeat(30)
+
 #creamos los objetos del videojuego
 bolita = Bolita()
+jugador = Paleta()
 
 while True:
     #establecer el tiempo del reloj
@@ -44,11 +71,15 @@ while True:
         #verificamos el evento de salir
         if evento.type == pygame.QUIT:
             sys.exit()
+        elif evento.type == pygame.KEYDOWN:
+            jugador.update(evento)
+            
     #mover la bolita
     bolita.update()
     #pintar la pantalla
     pantalla.fill(FONDO)
     #dibujamos los elementos del videjuego
     pantalla.blit(bolita.image,bolita.rect)
+    pantalla.blit(jugador.image,jugador.rect)
         
     pygame.display.flip()
