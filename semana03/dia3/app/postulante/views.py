e19d0ace24c444de16e3a3ed4972ec76bdb0bd93
César Mayta <cesarmayta@gmail.com>
2022-11-03 21:29:36 -0500

    dia3 statics

--- semana03/dia3/app/postulante/views.py
+++ semana03/dia3/app/postulante/views.py
@@ -5,3 +5,7 @@ from . import postulante
 @postulante.route('/')
 def index():
     return render_template('postulante/index.html')
+
+@postulante.route('/registro')
+def registro():
+    return render_template('postulante/registro.html')
