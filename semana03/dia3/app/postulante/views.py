9e4eba1a839bb98d148285681af873d041f18208
César Mayta <cesarmayta@gmail.com>
2022-11-03 21:54:21 -0500

    dia 3 layout postulante

--- semana03/dia3/app/postulante/views.py
+++ semana03/dia3/app/postulante/views.py
@@ -9,3 +9,11 @@ def index():
 @postulante.route('/registro')
 def registro():
     return render_template('postulante/registro.html')
+
+@postulante.route('/login')
+def login():
+    return render_template('postulante/login.html')
+
+@postulante.route('/editarperfil')
+def editarperfil():
+    return render_template('postulante/editar_perfil.html')
