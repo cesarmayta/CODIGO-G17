04bf3e8eba98cb00437c890d67a58552a7856cbf
César Mayta <cesarmayta@gmail.com>
2022-11-04 21:50:15 -0500

    dia4 select

--- semana03/dia4/app/__init__.py
+++ semana03/dia4/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+#conexion a base de datos
+from app.mysql_conn import MysqlDb
+mysqlDb = MysqlDb('localhost','root','','db_smartdevs')
+dbConn = mysqlDb.getConn()
+
+
 #blueprints
 from .portal import portal
 from .postulante import postulante
--- semana03/dia4/app/admin/views.py
+++ semana03/dia4/app/admin/views.py
@@ -2,6 +2,22 @@ from flask import Flask,render_template,request
 
 from . import admin
 
+from app import dbConn
+
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
+
+@admin.route('/area')
+def area():
+    cursor = dbConn.cursor(dictionary=True)
+    cursor.execute('select area_id as id,area_descripcion as descripcion from tbl_area')
+    data = cursor.fetchall()
+    print(data)
+    cursor.close()
+    
+    context = {
+        'areas':data
+    }
+    
+    return render_template('admin/area.html',**context)
