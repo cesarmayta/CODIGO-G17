d2e44dbaa8ce4d70cffbf0fb76b1341e3cc3707d
César Mayta <cesarmayta@gmail.com>
2022-11-09 21:52:22 -0500

    dia2 sql alchemy

--- semana04/dia2/app/__init__.py
+++ semana04/dia2/app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask,jsonify
+from flask_sqlalchemy import SQLAlchemy
 
+## importación de blueprints
 from .blueprints.ofertas import ofertas
-
+## parametros de configuración
+from .config import Config
 
 app = Flask(__name__)
+
+
+app.config.from_object(Config)
+app.app_context().push()
+
+db = SQLAlchemy(app)
+
 
 #registramos blueprints
 app.register_blueprint(ofertas)
---
+++ semana04/dia2/app/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+from os.path import join,dirname
+
+
+dotenv_path = join(dirname(__file__),'.env')
+load_dotenv(dotenv_path)
+
+class Config:
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DB_URI')
