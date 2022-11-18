6584871b2d8b5ca2d4a217513b7092d05c0c2e46
César Mayta <cesarmayta@gmail.com>
2022-11-17 23:00:25 -0500

    dia4 frontend-vanilla

--- semana05/dia4/frontend-vanilla/script.js
+++ semana05/dia4/frontend-vanilla/script.js
@@ -1,45 +1,36 @@
 function cargarTareas(){
-
-        //data de prueba
-        const productos = [{
-            id: 1,
-            nombre: "Preparar Clase React"
-        },
-        {
-            id: 2,
-            nombre: "Tomar Examen 4to ciclo"
-        }
-        ];
-
         fetch('http://localhost:5000/tarea')
-        .then((response) => response.json())
-        .then((data) => {
-            tareas = data.content;
-            console.log(data.content);
-            }
-            );
-
-        // Ahora dibujamos la tabla
-        const $cuerpoTabla = document.querySelector("#cuerpoTabla");
-        // Recorrer todos los productos
-        productos.forEach(producto => {
-                // Crear un <tr>
-                const $tr = document.createElement("tr");
-                // Creamos el <td> del codigo
-                let $tdId = document.createElement("th");
-                $tdId.textContent = producto.id; // el textContent del td es el nombre
-                $tr.appendChild($tdId);
-                // El td de nombre
-                let $tdNombre = document.createElement("td");
-                $tdNombre.textContent = producto.nombre;
-                $tr.appendChild($tdNombre);
-                // El td del botón eliminar
-                let $tdBotones = document.createElement("td");
-                $tdBotones.innerHTML = '<button type="submit" class="btn btn-danger">Eliminar</button>';
-                $tr.appendChild($tdBotones);
-                // Finalmente agregamos el <tr> al cuerpo de la tabla
-                $cuerpoTabla.appendChild($tr);
-                // Y el ciclo se repite hasta que se termina de recorrer todo el arreglo
+            .then(res => res.json())
+            .then((data) => {
+                tareas = data.content
+                console.log(tareas);
+            
+                // Ahora dibujamos la tabla
+                const $cuerpoTabla = document.querySelector("#cuerpoTabla");
+                // Recorrer todos los productos
+                tareas.forEach(tarea => {
+                        // Crear un <tr>
+                        const $tr = document.createElement("tr");
+                        // Creamos el <td> del codigo
+                        let $tdId = document.createElement("td");
+                        $tdId.textContent = tarea.id; // el textContent del td es el nombre
+                        $tr.appendChild($tdId);
+                        // El td de nombre
+                        let $tdNombre = document.createElement("td");
+                        $tdNombre.textContent = tarea.descripcion;
+                        $tr.appendChild($tdNombre);
+                        // El td de estado
+                        let $tdEstado = document.createElement("td");
+                        $tdEstado.textContent = tarea.estado;
+                        $tr.appendChild($tdEstado);
+                        // El td del botón eliminar
+                        //let $tdBotones = document.createElement("td");
+                        //$tdBotones.innerHTML = '<button type="submit" class="btn btn-danger">Eliminar</button>';
+                        //$tr.appendChild($tdBotones);
+                        // Finalmente agregamos el <tr> al cuerpo de la tabla
+                        $cuerpoTabla.appendChild($tr);
+                        // Y el ciclo se repite hasta que se termina de recorrer todo el arreglo
+                })
         });
 }
 cargarTareas();
