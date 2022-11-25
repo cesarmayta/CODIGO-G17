67b845bfb7a2ef596f870797b1c8dcc5712cb26f
César Mayta <cesarmayta@gmail.com>
2022-11-24 22:27:35 -0500

    dia4 models

--- semana06/dia4/smartdevs/smartdevs/settings.py
+++ semana06/dia4/smartdevs/smartdevs/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'smartdevs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_smartdevs',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
