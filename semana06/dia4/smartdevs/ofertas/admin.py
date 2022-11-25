76500083956a6743cf15048e443ec7006c4c036f
César Mayta <cesarmayta@gmail.com>
2022-11-24 23:02:01 -0500

    dia4 catalogo de ofertas

--- semana06/dia4/smartdevs/ofertas/admin.py
+++ semana06/dia4/smartdevs/ofertas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Area,Modalidad,Nivel,Periodo,Ubicacion,Oferta
+)
+
+admin.site.register(Area)
+admin.site.register(Modalidad)
+admin.site.register(Nivel)
+admin.site.register(Periodo)
+admin.site.register(Ubicacion)
+admin.site.register(Oferta)
