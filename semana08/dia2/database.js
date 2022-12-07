9a2e40fb7a78e56fab554ebfe0ac14529587a263
César Mayta <cesarmayta@gmail.com>
2022-12-06 20:43:50 -0500

    dia2 mysql

---
+++ semana08/dia2/database.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user :'root',
+    password:'',
+    database:'db_tareas_nodejs'
+})
+
+mysqlConnection.connect(function(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+    else{
+        console.log('conexión a base de datos exitosa')
+    }
+})
+
+mysqlConnection.query('select count(*) as total from tarea',function(error,results,fields){
+    if(error) throw error;
+    console.log('total de tareas : ',results[0].total);
+});
+
+mysqlConnection.end();
+
