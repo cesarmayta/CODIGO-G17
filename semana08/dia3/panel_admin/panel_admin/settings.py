7cfc3129000a0ca6c7b521e60dc1dda0bd518577
César Mayta <cesarmayta@gmail.com>
2022-12-09 22:50:59 -0500

    dia3 backoffice listo

--- semana08/dia3/panel_admin/panel_admin/settings.py
+++ semana08/dia3/panel_admin/panel_admin/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
--- semana08/dia3/panel_admin/panel_admin/urls.py
+++ semana08/dia3/panel_admin/panel_admin/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from django.conf import settings
-from django.conf.url.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
