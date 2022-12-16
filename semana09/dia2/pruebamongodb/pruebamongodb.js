053570d348b5c9ffb508c149e84512c72828ac21
César Mayta <cesarmayta@gmail.com>
2022-12-15 20:53:44 -0500

    insert one

--- semana09/dia2/pruebamongodb/pruebamongodb.js
+++ semana09/dia2/pruebamongodb/pruebamongodb.js
@@ -10,6 +10,9 @@ async function main(){
     const db = client.db('db_tareas');
     const collection = db.collection('tareas');
 
+    const insertar = await collection.insertOne({descripcion:"conectar mongodb con js",estado:"terminado"});
+    console.log('nueva tarea :',insertar);
+    
     const result = await collection.find({}).toArray();
     console.log('lista de tareas:',result);
     return 0;
