101ec34c16de7ede76707568bd6a5ec7daf839f7
cesar mayta <cesarmayta@gmail.com>
2022-12-16 22:43:04 -0500

    dia3 formulario tareas

--- semana09/dia3/frontend/src/App.jsx
+++ semana09/dia3/frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container,Row,Col,Card,Button,Table,Form} from 'react-bootstrap';
 import axios from 'axios';
 
 function App() {
+    const [tareas,setTareas] = useState([]);
+    const [descripcion,setDescripcion] = useState('');
+    const [estado,setEstado] = useState('pendiente');
+
+    useEffect(()=>{
+      axios.get('http://localhost:5000/tarea')
+      .then(res=>{
+        console.log(res.data.content);
+        setTareas(res.data.content);
+      })
+    },[])
+
     return(
       <>
       <Container>
@@ -11,21 +23,38 @@ function App() {
           <Col>
           <Card>
             <Card.Body>
-              <h2>Lista de Tareas</h2>
+              <center><h2>Lista de Tareas</h2></center>
+              <Col className="col-12">
+                <Form>
+                  <div className='form-outline'>
+                    <input type="text" className='form-control' onChange={(e)=>setDescripcion(e.target.value)} />
+                  </div>
+                  <div className='form-outline'>
+                    <Button type='submit' className='btn btn-primary'>Agregar Tarea</Button>
+                  </div>
+                </Form>
+              </Col>
               <Table>
                 <thead>
                   <tr>
                     <th scope="col">Nro.</th>
                     <th scope="col">Tarea</th>
+                    <th scope="col">Estado</th>
                     <th scope="col">Acciones</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>Tarea 1</td>
-                    <td></td>
-                  </tr>
+                  {
+                    tareas.map((tarea,index) =>(
+                      <tr key={tarea._id}>
+                        <td>{index + 1}</td>
+                        <td>{tarea.descripcion}</td>
+                        <td>{tarea.estado}</td>
+                        <td></td>
+                      </tr>
+                    ))
+                  }
+                  
                 </tbody>
               </Table>
             </Card.Body>
