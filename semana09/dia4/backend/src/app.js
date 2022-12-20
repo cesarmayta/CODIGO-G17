d8156c1c220e6100e565591003ca6279f744f4aa
César Mayta <cesarmayta@gmail.com>
2022-12-19 20:48:53 -0500

    dia4 proyecto base

---
+++ semana09/dia4/backend/src/app.js
@@ -0,0 +1,19 @@
+const express  = require('express');
+const {config} = require('./config');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.set('port',config.port);
+
+app.get('/',(req,res)=>{
+    res.json({
+        "success": true,
+        "message": "api rest de ecommerce"
+    })
+})
+
+module.exports = app;
