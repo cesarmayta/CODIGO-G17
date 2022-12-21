98bdc82289c8357983148d4aba9aab749118f522
César Mayta <cesarmayta@gmail.com>
2022-12-20 21:55:22 -0500

    upload images

--- semana09/dia5/backend/src/controllers/product.controller.js
+++ semana09/dia5/backend/src/controllers/product.controller.js
@@ -2,6 +2,15 @@ const productController = {}
 
 const {config} = require('../config');
 
+//para conexión a cloudinary
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name: config.cloudinary.cloud_name,
+    api_key: config.cloudinary.api_key,
+    api_secret:config.cloudinary.api_secret
+})
+
 const productModel = require('../models/product.model');
 
 
@@ -31,6 +40,35 @@ productController.create = async (req,res)=>{
             content:error
         })
     }
+}
+
+productController.uploadImage = async (req,res)=>{
+
+    productImage = req.files.productImage
+    console.log(productImage.name);
+
+    let uploadPath;
+    uploadPath = '../backend/src/uploads' + productImage.name;
+
+    await productImage.mv(uploadPath,function(err){
+        if(err){
+            res.status(502).json({
+                success:false,
+                message: "upload image error",
+                content: err
+            })
+        }
+        else{
+            cloudinary.uploader.upload(uploadPath,(error,result)=>{
+                console.log(result,error);
+                res.json({
+                    success:true,
+                    message:"upload successfully",
+                    content: result.url
+                })
+            })
+        }
+    })
 }
 
 module.exports = productController;
