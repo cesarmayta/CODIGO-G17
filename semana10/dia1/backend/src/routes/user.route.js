9cc7e8981ac750faf83b879ae94247d2f74952b9
César Mayta <cesarmayta@gmail.com>
2022-12-21 22:48:02 -0500

    dia1 jwt

--- semana10/dia1/backend/src/routes/user.route.js
+++ semana10/dia1/backend/src/routes/user.route.js
@@ -1,9 +1,12 @@
 const {Router} = require('express');
 const router = Router();
 
-const {create} = require('../controllers/user.controller');
+const {create,auth} = require('../controllers/user.controller');
 
 router.route('/')
     .post(create)
+
+router.route('/auth')
+    .post(auth)
 
 module.exports = router;
