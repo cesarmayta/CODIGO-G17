e4e32a11b9e20820d8fdb70e60d86f70d6cbb2c7
César Mayta <cesarmayta@gmail.com>
2022-12-22 20:09:28 -0500

    auth token

--- semana10/dia2/backend/src/middlewares/auth.handler.js
+++ semana10/dia2/backend/src/middlewares/auth.handler.js
@@ -6,6 +6,19 @@ function verifyToken(req,res,next){
 
     if(typeof bearerToken !== 'undefined'){
         //validamos el token
+        const bearer = bearerToken.split(' ')
+        const token = bearer[1];
+        console.log('jwt : ',token)
+        try{
+            const decoded = jwt.verify(token,'qwerty123')
+            console.log(decoded);
+        }catch(err){
+            return res.status(401).json({
+                success:false,
+                message:'error al validar token',
+                content: err
+            })
+        }
         return next();
     }else{
         res.status(403).json({
