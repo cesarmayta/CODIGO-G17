029c0ef7bdfc1ef70827a9dc0796ff1b01492730
César Mayta <cesarmayta@gmail.com>
2022-12-22 22:53:11 -0500

    dia2 cambios mongoose models

--- semana10/dia2/backend/src/models/category.model.js
+++ semana10/dia2/backend/src/models/category.model.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
 const CategorySchema = new Schema({
+    id:{
+        type:Number
+    },
     name:{
         type:String,
         required:true
@@ -10,5 +15,7 @@ const CategorySchema = new Schema({
     timestamps:false,
     versionKey:false
 })
+
+CategorySchema.plugin(AutoIncrement, {inc_field: 'id'});
 
 module.exports = mongoose.model('categories',CategorySchema);
