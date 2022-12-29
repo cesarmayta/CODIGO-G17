e131ad2e231b08dacc16fcc6e1a5a2c1ffce9994
César Mayta <cesarmayta@gmail.com>
2022-12-28 21:51:24 -0500

    dia4 views

--- semana10/dia4/backend/apimiembros/api/views.py
+++ semana10/dia4/backend/apimiembros/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
 
-# Create your views here.
+from .models import *
+from .serializers import *
+
+class CategoriaView(generics.ListAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+class CursoView(generics.ListAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
